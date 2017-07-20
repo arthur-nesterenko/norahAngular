b6bfe9d645056e99ccff2ab1d7701b9096d53644
Roman Kysenko <rky@ciklum.com>
2017-07-20 16:14:35 +0300

    My library fix

--- src/app/pages/library/library.component.ts
+++ src/app/pages/library/library.component.ts
@@ -23,7 +23,7 @@ export class LibraryComponent implements AfterViewInit {
         let animationsArray: Array<{}> | void = [];
         let k = 1;
         const userId = firebase.auth().currentUser.uid;
-        firebase.database().ref('usernames').child(userId).child('mylibrary').once('value', function (ss) {
+        firebase.database().ref('usernames').child(userId).child('mylibrary').orderByValue().once('value', function (ss) {
           const animations = ss.val();
           if (!animations) {
             alert('No items in library');
@@ -45,15 +45,13 @@ export class LibraryComponent implements AfterViewInit {
               anim.animUrl = animFileUrl;
 
               return anim;
-            }).sort(function (anim1, anim2) {
-              return (anim1['displayName']).localeCompare(anim2['displayName']);
             }).forEach(function (anim) {
               blocks += '<div class="box-video box' + k + ' fadeInUp clust" data-wow-delay="0.3s" data-page="#">';
               blocks += '<div style="z-index: 111;">';
-              blocks += '<div class="animation-name" style="text-align:center;margin-top:40px;display:block">' + anim.displayName + '</div>';
+              blocks += '<div class="animation-name" style="text-align:center;margin-top:40px;display:block">' + anim.name + '</div>';
               blocks += '<a class="download-anim" target="_blank" href="' + anim.mp4Url + '" data-url="' + anim.animUrl + '" data-duration="' + anim.duration + '" data-name="' + anim.name + '.anim" style="float:none !important;text-align:center;display:block;margin-top:0px"><br/><i class="fa fa-download fa-2x" aria-hidden="true"></i></a></center>';
-              blocks += '<label class="fancy-checkbox">';
-              blocks += '<input type="checkbox" name="demo_' + anim.firebaseKey + '" click="if(this.checked){ document.getElementById(' + k + ').checked = true;} else {document.getElementById(' + k + ').checked = false;}"/>';
+              blocks += '<label class="fancy-checkbox library-checkbox">';
+              blocks += '<input  type="checkbox" name="demo_' + anim.firebaseKey + '" click="if(this.checked){ document.getElementById(' + k + ').checked = true;} else {document.getElementById(' + k + ').checked = false;}"/>';
               blocks += '<span></span>';
               blocks += '</label>';
               blocks += '</div>';
