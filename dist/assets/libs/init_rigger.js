373f9bd49e8070ff74440ea6f6fd140001cd80ee
Norah Absentia <vrushali@absentiavr.com>
2017-07-27 20:01:15 +0530

    Added Travis-CI

---
+++ dist/assets/libs/init_rigger.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+
+  // var socket = io.connect('https://absentiaautorig.com:443');
+
+
+
+  /*
+   setTimeout(loaderPause, 15000);
+
+   function loaderPause() {
+   document.getElementById("loader").style.display = "none";
+   }
+   */
+
+
+  //document.getElementById('js-upload-button').click();
+
+};
