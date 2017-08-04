c211eacefe09e0df19f71b5308d149cb5892e9b8
Vrushali <vrushali@absentiavr.com>
2017-08-04 16:58:26 +0530

    Service fix & mylib change notice

--- src/app/pages/terrain-gen/terrain-gen.service.ts
+++ src/app/pages/terrain-gen/terrain-gen.service.ts
@@ -1,12 +1,19 @@
 import {Inject, Injectable} from '@angular/core';
 import { FirebaseApp } from 'angularfire2/angularfire2';
 import * as firebase from 'firebase';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { GlobalRef } from '../../global-ref';
 
 @Injectable()
 export class TerrainGenService {
 
   private terrainsArr: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
-
+  private terrains: BehaviorSubject<string[]> = new BehaviorSubject([]);
+  private terrainType: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  terrains$: Observable<string[]> = this.terrains.asObservable();
+  constructor(@Inject(FirebaseApp) private firebaseApp, private global: GlobalRef) {
+  }
 
   getTerrains(type: string) {
     const terrainsArr = [];
@@ -23,13 +30,6 @@ export class TerrainGenService {
       .then(data => data);
   }
 
-
-  private terrains: BehaviorSubject<string[]> = new BehaviorSubject([]);
-  private terrainType: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  terrains$: Observable<string[]> = this.terrains.asObservable();
-  constructor(@Inject(FirebaseApp) private firebaseApp, private global: GlobalRef) {
-  }
-
   selectTerrain(terrainType) {
     this.terrainType.next(terrainType);
   }
@@ -40,13 +40,14 @@ export class TerrainGenService {
     firebase.database()
       .ref('usernames')
       .child(userId)
-      .child('mylibrary/')
+      .child('mylibrary/')  // Change this to myterrainlibrary, it gets mixed up with another lib in fb
       .once('value', snapshot => {
         const value = snapshot.val();
         const existing = Object.keys(value).some((key: string) => terrain === value[key].terrain);
         if (existing) {
           toastr.error('Already in your library');
         } else {
+          // Change this to myterrainlibrary, it gets mixed up with another lib in fb⬇︎
           const newObjRef = firebase.database().ref('usernames').child(userId).child('mylibrary/').push();
           newObjRef.set({
             terrain: terrain
