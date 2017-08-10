87480d9959be37b8bb5d5a883df50ae216f30f20
Irek Gubaidullin <44irek@gmail.com>
2017-08-10 13:10:45 +0500

    Texture

--- src/app/pages/gun-interp/gun-interp.component.ts
+++ src/app/pages/gun-interp/gun-interp.component.ts
@@ -13,7 +13,29 @@ export class GunInterpComponent implements OnInit {
   isGun: boolean = true;
   isGenerate: boolean = false;
   isTexture: boolean = false;
-
+  openTexture() {
+    if ( this.isTexture ) {
+      this.isTexture = false;
+      this.isGun = true;
+      this.isGenerate = false;
+    } else {
+      this.isTexture = true;
+      this.isGun = false;
+      this.isGenerate = false;
+    }
+    
+  }
+  openGeneration() {
+    if ( this.isGenerate ) {
+      this.isTexture = false;
+      this.isGun = true;
+      this.isGenerate = false;
+    } else {
+      this.isTexture = false;
+      this.isGun = false;
+      this.isGenerate = true;
+    }
+  }
   ngOnInit() {
   }
 
