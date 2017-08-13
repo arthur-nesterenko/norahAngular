47e2a1cd08701430196fd05f5598d1351949c59d
Mekey Salaria <degroundshaker@gmail.com>
2017-08-13 18:03:08 +0530

    Export  Game Page Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BrowserGlobalRef, GlobalRef } from './global-ref';
 import { AboutComponent } from './pages/about/about.component';
 import { AuthModule } from './pages/auth/auth.module';
 import { DialogService } from './pages/auth/dialog/dialog.component';
+import { ExportGameComponent } from './pages/export-game/export-game.component';
 import { CharMakerComponent } from './pages/char-maker/char-maker.component';
 import { DnaCodeComponent } from './pages/dna-code/dna-code.component';
 import { HomeModule } from './pages/home/home.module';
@@ -26,6 +27,7 @@ import { GunInterpService } from './pages/gun-interp/gun-interp.service';
   declarations: [
     AppComponent,
     AboutComponent,
+	ExportGameComponent,
     CharMakerComponent,
     DnaCodeComponent,
   ],
---
+++ src/app/pages/export-game/export-game.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'export-game',
+  templateUrl: './export-game.component.html',
+  styleUrls: ['./export-game.component.scss']
+})
+export class ExportGameComponent {
+
+  constructor() {
+  }
+	
+	
+
+}
