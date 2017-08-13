8b0229e6c44ecd50b2e4e730e54e6196d4c8bc2d
Roman Kysenko <rky@ciklum.com>
2017-08-13 11:55:58 +0300

    Display "displayName" on the animation in the repository instead of "name" key.

--- src/app/pages/repository/repository.component.ts
+++ src/app/pages/repository/repository.component.ts
@@ -5,7 +5,6 @@ import 'rxjs/add/operator/map';
 import { GlobalRef } from '../../global-ref';
 import { RepositoryService } from './repository.service';
 
-
 @Component({
   selector: 'bc-repository',
   templateUrl: './repository.component.html',
@@ -19,14 +18,13 @@ export class RepositoryComponent implements OnInit, AfterViewInit {
   page = 1;
   keyword = '';
   animationsCount = 0;
-
-
+  
   constructor(private repService: RepositoryService, private global: GlobalRef) {
     repService.unselectedTags$.subscribe(tag => {
       this.removeTag(tag);
     });
   }
-
+  
   ngOnInit() {
     const arr = [];
     this.animations = arr;
@@ -34,7 +32,10 @@ export class RepositoryComponent implements OnInit, AfterViewInit {
     this.selectedTags = [];
     this.repService.animations
       .subscribe((result: Animation[]) => {
-      console.log(result);
+      result = result.sort((a, b) => {
+        return a.displayName.localeCompare(b.displayName);
+      });
+        console.log(result);
         this.animations = result;
         this.filterAnimations();
       });
@@ -45,57 +46,57 @@ export class RepositoryComponent implements OnInit, AfterViewInit {
     this.repService.tags.subscribe((tags: Tag[string]) => {
       this.tags = tags.map((tag: Tag) => {
         delete tag.$exists;
-
+        
         const store = [];
-        for (const i in tag) {
-          if (i !== '$key') {
+        for ( const i in tag ) {
+          if ( i !== '$key' ) {
             store.push(tag[i]);
           }
         }
-        return {key: tag['$key'], tags: store};
+        return { key: tag['$key'], tags: store };
       });
     });
   }
-
+  
   setPage(page) {
     this.repService.nextPage(page);
   }
-
+  
   addTag(tag) {
     this.repService.nextPage(1);
-    if (!this.selectedTags.includes(tag)) {
+    if ( !this.selectedTags.includes(tag) ) {
       this.selectedTags.push(tag);
       this.repService.addTag(tag);
     }
     this.filterAnimations();
   }
-
+  
   removeTag(tag) {
     this.repService.nextPage(1);
     this.selectedTags.splice(this.selectedTags.indexOf(tag), 1);
     this.filterAnimations();
   }
-
+  
   filterAnimations() {
     const selectedTags = this.selectedTags;
-    if (!selectedTags.length) {
+    if ( !selectedTags.length ) {
       this.animationsCount = this.animations.length;
       this.displayAnimations = this.animations.slice((this.page - 1) * 15, (this.page - 1) * 15 + 15);
     }
     const arrayLength = selectedTags.length;
     const anim_final = [];
-    if (arrayLength > 0 && !$.isEmptyObject(this.animations)) {
+    if ( arrayLength > 0 && !$.isEmptyObject(this.animations) ) {
       this.animations.forEach(function (anim) {
-
+        
         let count = 0;
-        for (const t in anim['tags']) {
-          for (let i = 0; i < arrayLength; i++) {
-            if (t === selectedTags[i]) {
+        for ( const t in anim['tags'] ) {
+          for ( let i = 0; i < arrayLength; i++ ) {
+            if ( t === selectedTags[i] ) {
               count++;
             }
           }
         }
-        if (count === arrayLength) {
+        if ( count === arrayLength ) {
           anim_final.push(anim);
         }
       });
@@ -105,17 +106,17 @@ export class RepositoryComponent implements OnInit, AfterViewInit {
     this.animationsCount = anim_final.length;
     this.displayAnimations = anim_final.slice((this.page - 1) * 15, (this.page - 1) * 15 + 15);
   }
-
+  
   checkTag(tag, array) {
     const a = array.filter((item) => item.tags[tag]);
     return a.length === array.length;
   }
-
+  
   addVideo(animation) {
     const wnd = this.global.nativeGlobal;
     const toastr = wnd.toastr;
     const download = wnd.download;
-    if (firebase.auth().currentUser) {
+    if ( firebase.auth().currentUser ) {
       const animName = animation.name;
       const duration = animation.duration;
       const displayName = animation.displayName;
@@ -128,17 +129,17 @@ export class RepositoryComponent implements OnInit, AfterViewInit {
         Object.keys(libraryItems).forEach(function (itemKey) {
           exists = libraryItems[itemKey]['name'] === animName;
         });
-        if (!exists) {
+        if ( !exists ) {
           const newObjRef = firebase.database().ref('usernames').child(userId).child('mylibrary/').push();
-
+          
           const storageBucket = (firebase.app().options as any).storageBucket;
           const animMp4Name = 'mp4Files/' + animName + '.mp4';
           const mp4Url = `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/${encodeURIComponent(animMp4Name)}?alt=media`;
-
+          
           const animFileName = 'animFiles/' + animName + '.anim';
           const animFileUrl = `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/
             ${encodeURIComponent(animFileName)}?alt=media`;
-
+          
           newObjRef.set({
             displayName: displayName,
             name: animName,
@@ -158,21 +159,21 @@ export class RepositoryComponent implements OnInit, AfterViewInit {
       });
     });
   }
-
+  
   matchTags() {
     const arrayLength = this.selectedTags.length;
     const anim_final = [];
-    if (arrayLength > 0 && !$.isEmptyObject(this.displayAnimations)) {
+    if ( arrayLength > 0 && !$.isEmptyObject(this.displayAnimations) ) {
       this.displayAnimations.forEach((anim) => {
         let count = 0;
-        for (const t in anim['tags']) {
-          for (let i = 0; i < arrayLength; i++) {
-            if (t === this.selectedTags[i]) {
+        for ( const t in anim['tags'] ) {
+          for ( let i = 0; i < arrayLength; i++ ) {
+            if ( t === this.selectedTags[i] ) {
               count++;
             }
           }
         }
-        if (count === arrayLength) {
+        if ( count === arrayLength ) {
           anim_final.push(anim);
         }
       });
@@ -181,46 +182,45 @@ export class RepositoryComponent implements OnInit, AfterViewInit {
     }
     return anim_final;
   }
-
+  
   KeywordChanged(text) {
     this.keyword = text.toLowerCase();
   }
-
+  
   isEmpty() {
     return this.keyword.length <= 0;
   }
-
+  
   getKeyWord() {
-
+    
     return this.keyword;
   }
-
+  
   searchKeywords(anims) {
-    if (this.keyword.length > 0) {
+    if ( this.keyword.length > 0 ) {
       const filteredAnims = [];
       anims.filter((anim) => {
         const k = this.keyword.split(' ');
-        for (let i = 0; i < k.length; i++) {
-          if (anim.displayName.toLowerCase().indexOf(k[i]) >= 0) {
+        for ( let i = 0; i < k.length; i++ ) {
+          if ( anim.displayName.toLowerCase().indexOf(k[i]) >= 0 ) {
             console.log('Key: ' + k + ' name ' + anim.displayName);
             filteredAnims.push(anim);
             return anim;
-
+            
           }
         }
-
+        
         return false;
-
+        
       });
       return filteredAnims;
     } else {
       return anims;
     }
   }
-
+  
   ngAfterViewInit() {
-
-
+  
   }
 }
 
@@ -232,6 +232,7 @@ export interface Animation {
   jsonUrl: string;
   mp4Url: string;
   name: string;
+  displayName: string;
   yamlUrl: string;
   tags: Tag[];
 }
@@ -239,6 +240,6 @@ export interface Animation {
 export interface Tag {
   $exist?: Function;
   $key?: string;
-
+  
   [key: string]: any;
 }
